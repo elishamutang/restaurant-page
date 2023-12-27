c24cbc401c0f749399d03f16dc6a14f847c221a5
elishamutang <elishamutang@outlook.com>
2023-12-27 14:52:35 +0800

    Configured html webpack plugin for this project.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant page',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+    ],
 };
