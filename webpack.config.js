8b9a43758a775c87e0b38bcbce999f4b16355d25
elishamutang <elishamutang@outlook.com>
2023-12-27 14:56:15 +0800

    Configured webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
