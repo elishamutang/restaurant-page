20736c80d1bcdc99f36354e289b176da67d50da3
elishamutang <elishamutang@outlook.com>
2023-12-31 16:24:26 +0800

    Initialized styling for header component.

--- src/header.js
+++ src/header.js
@@ -1,5 +1,18 @@
 // Module for generating and appending header to webpage.
 
+// Main header
 export const header = document.createElement("div");
 header.className = "header";
 header.id = "header";
+
+// Tabs on header div
+const tabDiv = document.createElement('div');
+tabDiv.className = 'header';
+tabDiv.id = 'tabDiv';
+
+header.append(tabDiv);
+
+// Logo div
+const logoDiv = document.createElement('div');
+logoDiv.className = 'header';
+logoDiv.id = 'logoDiv';
