308f25d35a32e0e17ebe4c5275bf67fa3e3e7918
elishamutang <elishamutang@outlook.com>
2023-12-31 15:39:13 +0800

    Initialized layout of webpage.

--- src/content.js
+++ src/content.js
@@ -1,2 +1,7 @@
 // Content after header
 
+export const content = document.createElement('div');
+content.className = 'content';
+content.id = 'content';
+
+content.textContent = 'Testing, this is a main content div';
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
 
 import styles from './styles.css';
 import { header } from './header.js';
+import { content } from './content.js';
 
 // Target content div
 const contentDiv = document.querySelector('#mainContainer');
 
+// Append different sections to main web page.
 contentDiv.append(header);
+contentDiv.append(content);
