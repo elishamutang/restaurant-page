acc53e76eec9a93c727dd8c85a5871ead751b1a4
elishamutang <elishamutang@outlook.com>
2024-01-01 20:24:36 +0800

    Some progress I forgot to save on 31/12/23

--- src/header.js
+++ src/header.js
@@ -5,14 +5,17 @@ export const header = document.createElement("div");
 header.className = "header";
 header.id = "header";
 
+// Logo div
+const logoDiv = document.createElement('div');
+logoDiv.className = 'header';
+logoDiv.id = 'logoDiv';
+logoDiv.textContent = 'Oriental Kopi';
+
+header.append(logoDiv);
+
 // Tabs on header div
 const tabDiv = document.createElement('div');
 tabDiv.className = 'header';
 tabDiv.id = 'tabDiv';
 
 header.append(tabDiv);
-
-// Logo div
-const logoDiv = document.createElement('div');
-logoDiv.className = 'header';
-logoDiv.id = 'logoDiv';
