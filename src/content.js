9a17593aad8712a3f949c1e95cf45572000e58aa
elishamutang <elishamutang@outlook.com>
2024-01-03 08:39:45 +0800

    Fix content.js image not loading in webpage.

--- src/content.js
+++ src/content.js
@@ -5,3 +5,12 @@ content.className = 'content';
 content.id = 'content';
 
 content.textContent = 'Testing, this is a main content div';
+
+// Oriental kopi picture
+const pictureDiv = document.createElement('div');
+pictureDiv.id = 'picture';
+
+const image = document.createElement('img');
+image.src = './assets/oriental_picture.jpeg';
+
+pictureDiv.append(image);
