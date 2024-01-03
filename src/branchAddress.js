f37b37b0d7849617c9c73dbd610c5669f26ea88f
elishamutang <elishamutang@outlook.com>
2024-01-03 14:00:05 +0800

    Changed from home to branch address tab.

---
+++ src/branchAddress.js
@@ -0,0 +1,13 @@
+// Homepage
+import orientalImg from './assets/oriental_picture.jpeg';
+
+// Oriental kopi picture
+export const pictureDiv = document.createElement('div');
+pictureDiv.id = 'picture';
+
+const image = document.createElement('img');
+image.src = orientalImg;
+
+pictureDiv.append(image);
+
+// Branch address section
