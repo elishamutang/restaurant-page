d378da4bfe5798c26655c9a0b9790df25db11b10
elishamutang <elishamutang@outlook.com>
2024-01-20 14:32:37 +0800

    [refactor] Re-factored menuBar.js

--- src/menuBar.js
+++ src/menuBar.js
@@ -1,15 +1,23 @@
 // Menu bar for webpage.
+export default function navBar() {
 
-const menuBarItems = ['Home', 'Menu', 'Products', 'Outlets'];
-export const menuBarDivs = [];
+    const menuBarItems = ['Home', 'Menu', 'Products', 'Outlets'];
+    const menuBarDivs = [];
+    const tabDiv = document.querySelector('#tabDiv'); 
 
-for(let i = 0; i < menuBarItems.length; i++) {
-    const item = document.createElement('div');
-    item.dataset.index = `${i}`;
-    item.className = 'menu-item';
-    menuBarDivs.push(item);
+    for(let i = 0; i < menuBarItems.length; i++) {
+        const item = document.createElement('div');
+        item.dataset.index = `${i}`;
+        item.className = 'menu-item';
+        menuBarDivs.push(item);
 
-    const itemLink = document.createElement('a');
-    itemLink.textContent = `${menuBarItems[i]}`;
-    menuBarDivs[i].append(itemLink);
+        const itemLink = document.createElement('a');
+        itemLink.textContent = `${menuBarItems[i]}`;
+        menuBarDivs[i].append(itemLink);
+    }
+
+    menuBarDivs.forEach((div) => {
+        tabDiv.append(div);
+    });
+
 }
