4b4c58c513d308e656ab8f5596721f089d3a8243
elishamutang <elishamutang@outlook.com>
2024-01-21 21:26:39 +0800

    [feature] Added menuItems function to generate menu

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,23 @@
 import { mainContainer } from "./index.js";
 import menuImage from "./assets/randomHeaderImgTwo.jpg";
 import headingFlower from "./assets/heading-flower.png";
+import { linkImgs } from "./home.js";
+import nasiLemak from "./assets/nasi-lemak.jpg";
+import malayRendangChicken from "./assets/malay-rendang-chicken.jpg";
+import meeSiam from "./assets/mee-siam.jpg";
+import chickenHorFun from "./assets/chicken-hor-fun.jpg";
+import charKueyTeow from "./assets/char-kuey-teow.jpg";
+import curryLaksaMee from "./assets/curry-laksa-mee.jpg";
+import nanyangCurry from "./assets/nanyang-curry.jpg";
+import hainaneseChickenRice from "./assets/hainanese-chicken-rice.jpg";
+import malayFriedRice from "./assets/malay-fried-rice.jpg";
+import prawnFriedRice from "./assets/prawn-fried-rice.jpg";
+import signatureEggTart from "./assets/signature-egg-tart.jpg";
+import poloBun from "./assets/polo-bun.jpg";
+import doubleButterKayaToast from "./assets/double-butter-kaya-toast.jpg";
+import rojak from "./assets/rojak.jpg";
+import orientalKopi from "./assets/oriental-kopi.jpg";
+import cendol from "./assets/cendol.jpg";
 
 export default function menuPage() {
 
@@ -57,7 +74,33 @@ function signatureSect() {
     lastFlowerDiv.insertAdjacentElement('beforebegin', signatureTitle);
 
     signatureMenu.append(signatureHeader);
+    signatureMenu.append(menuItems());
 
     return signatureMenu;
+
+}
+
+function menuItems() {
+
+    const foodNames = ['Nasi Lemak', 'Malay Rendang Chicken', 'Mee Siam', 'Chicken Hor Fun', 'Char Kuey Teow', 'Curry Laksa Mee',
+                        'Nanyang Curry', 'Hainanese Chicken Rice', 'Malay Fried Rice', 'Prawn Fried Rice', 'Signature Egg Tart',
+                        'Polo Bun', 'Double Butter Kaya Toast', 'Rojak', 'Oriental Kopi', 'Cendol'];
+
+    const foodImgs = [nasiLemak, malayRendangChicken, meeSiam, chickenHorFun, charKueyTeow, curryLaksaMee, nanyangCurry,
+                        hainaneseChickenRice, malayFriedRice, prawnFriedRice, signatureEggTart, poloBun, doubleButterKayaToast,
+                        rojak, orientalKopi, cendol];
+
+    // Create 4x4 container to display each food.
+    const foodItems = linkImgs(foodNames.length, foodImgs, foodNames.length, 'foodItem', 'foodItems');
+
+    const foodContainer = document.createElement('div');
+    foodContainer.id = 'foodContainer';
+
+    foodItems.forEach((item) => {
+        console.log(item);
+        foodContainer.append(item);
+    })
+
+    return foodContainer;
 
 }
