766d8f5608f5c60dca720a81a5d9c1b88763d106
elishamutang <elishamutang@outlook.com>
2024-01-21 14:42:59 +0800

    [feature] Initial menuPage function.

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,17 @@
 // Menu page
 import { mainContainer } from "./index.js";
-
+import menuImage from "./assets/randomHeaderImgTwo.jpg";
 
 export default function menuPage() {
 
-    
+    const menuImgDiv = document.createElement('div');
+    menuImgDiv.id = 'menuImgDiv';
+    menuImgDiv.className = 'headerImg';
+
+    const menuImgTag = document.createElement('img');
+    menuImgTag.src = menuImage;
+
+    menuImgDiv.append(menuImgTag);
+    mainContainer.append(menuImgDiv);
 
 }
