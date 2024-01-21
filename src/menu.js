c794f342bf902bcf7a5f69c0b8f0220311442055
elishamutang <elishamutang@outlook.com>
2024-01-21 17:20:54 +0800

    [refactor] Finished signature menu header.

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,11 @@
 // Menu page
 import { mainContainer } from "./index.js";
 import menuImage from "./assets/randomHeaderImgTwo.jpg";
+import headingFlower from "./assets/heading-flower.png";
 
 export default function menuPage() {
 
+    // Header image
     const menuImgDiv = document.createElement('div');
     menuImgDiv.id = 'menuImgDiv';
     menuImgDiv.className = 'headerImg';
@@ -13,5 +15,41 @@ export default function menuPage() {
 
     menuImgDiv.append(menuImgTag);
     mainContainer.append(menuImgDiv);
+    
+    // Signature menu section
+    const signatureMenu = document.createElement('div');
+    signatureMenu.id = 'signatureMenu';
+    signatureMenu.className = 'non-img';
+
+    // Signature menu heading
+    const signatureHeader = document.createElement('div');
+    signatureHeader.className = 'signatureHeader';
+
+    const signatureTitle = document.createElement('h1');
+    signatureTitle.textContent = 'Signature Menu';
+    signatureTitle.className = 'headerTitle';
+
+    const flowerDivs = [];
+
+    for(let i=0; i<2; i++) {
+
+        const flowerImg = document.createElement('img');
+        flowerImg.src = headingFlower;
+
+        const flowerImgDiv = document.createElement('div');
+        flowerImgDiv.className = 'flowerImg';
+
+        flowerImgDiv.append(flowerImg);
+
+        flowerDivs.push(flowerImgDiv);
+        signatureHeader.append(flowerImgDiv);
+
+    }
+
+    const lastFlowerDiv = flowerDivs[1];
+    lastFlowerDiv.insertAdjacentElement('beforebegin', signatureTitle);
+
+    signatureMenu.append(signatureHeader);
+    mainContainer.append(signatureMenu);
 
 }
